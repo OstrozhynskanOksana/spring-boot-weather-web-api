c47c5e292fad4e1730356cad5438a6871fb9ccf0
oksana <ostroginskaa@gmail.com>
2026-02-01 13:52:29 +0200

    innit new project

---
+++ src/test/java/com/example/weatherspringboot/WeatherSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

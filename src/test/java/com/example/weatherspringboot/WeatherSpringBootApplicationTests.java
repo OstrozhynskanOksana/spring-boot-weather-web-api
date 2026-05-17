bdfd0fee373cc388129169b2e3d9a029fab80d46
oksana <ostroginskaa@gmail.com>
2026-05-17 19:38:13 +0300

    chore: align docker and test configuration

--- src/test/java/com/example/weatherspringboot/WeatherSpringBootApplicationTests.java
+++ src/test/java/com/example/weatherspringboot/WeatherSpringBootApplicationTests.java
@@ -3,7 +3,13 @@ package com.example.weatherspringboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:weather-test",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "jwt.secret=MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI=",
+        "jwt.expiration=60"
+})
 class WeatherSpringBootApplicationTests {
 
     @Test
